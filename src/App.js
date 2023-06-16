308fed9ef070e8705f1d824f8571dce79f2543f2
Akins20 <ogunbiye@gmail.com>
2023-06-16 13:04:31 +0100

    Added Masonry assets

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from './components/header';
+import Slider from './components/slider';
+import Homebody from './components/homebody';
 
 function App() {
   return (
     <div>
       <Header/>
+      <Slider/>
+      <Homebody/>
     </div>
   );
 }
---
+++ src/components/slider.js
@@ -0,0 +1,23 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+import React from "react";
+import { Carousel } from "3d-react-carousal";
+
+const Slider = function () {
+    let slides = [
+        <img src="https://picsum.photos/800/300/?random" alt="1" />,
+        <img src="https://picsum.photos/800/301/?random" alt="2" />,
+        <img src="https://picsum.photos/800/302/?random" alt="3" />,
+        <img src="https://picsum.photos/800/303/?random" alt="4" />,
+        <img src="https://picsum.photos/800/304/?random" alt="5" />];
+    const callback = function (index) {
+        console.log("callback", index);
+    }
+    return (
+        <div className="bg-dark text-light">
+            <h1 className="p-3">Featured</h1>
+            <Carousel slides={slides} autoplay={true} interval={3000} onSlideChange={callback} />
+        </div>
+    )
+}
+
+export default Slider;
