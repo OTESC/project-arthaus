b4209bfdbabbb3c39723339facf756bbbecf3de0
Akins20 <ogunbiye@gmail.com>
2023-06-23 19:34:38 +0100

    .

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import Header from './components/header';
 import Slider from './components/slider';
 import Homebody from './components/homebody';
-import PersonalDetails from './components/personalDetails';
-import FrequentlyAskedQuestions from './components/frequentQuestions';
 
 function App() {
   return (
