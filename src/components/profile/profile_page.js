6058273945b2612795b47b4981e7e7a4dc325c28
TaiwoTom <taiwotom8@gmail.com>
2023-07-05 23:19:22 +0100

    profile commit

---
+++ src/components/profile/profile_page.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../header";
+import AddPost from "./add_post";
+import PostsSaved from "./posts_saved";
+import Settings from "./settings";
+import UserBio from "./user_bio";
+import "./profile.css"
+import "../../bootstrap-5.3.0-alpha3-dist/css/bootstrap.css";
+const ProfilePage = () => {
+  return (
+    <div>
+      <Header />
+      <UserBio />
+      <Settings />
+      <AddPost />
+      <PostsSaved />
+    </div>
+  );
+};
+export default ProfilePage;
