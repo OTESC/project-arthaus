aaf62391fea9456839c3ddb26ebb87c5b55c2ebd
Akins20 <ogunbiye@gmail.com>
2023-07-05 23:31:54 +0100

    Revert "anot the"
    This reverts commit 298a98df9c15798eb6dbebfb2a596becc2457416.

--- src/Newfile.js
+++ src/Newfile.js
@@ -2,11 +2,4 @@ import react from "react"
 
 export default function water() {
 
-    return(
-        <div>
-            <p>THis isjust a test .
-                Will it work
-            </p>
-        </div>
-    )
 }
