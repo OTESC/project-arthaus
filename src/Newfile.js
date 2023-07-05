298a98df9c15798eb6dbebfb2a596becc2457416
Akins20 <ogunbiye@gmail.com>
2023-07-05 23:17:52 +0100

    anot the

--- src/Newfile.js
+++ src/Newfile.js
@@ -2,4 +2,11 @@ import react from "react"
 
 export default function water() {
 
+    return(
+        <div>
+            <p>THis isjust a test .
+                Will it work
+            </p>
+        </div>
+    )
 }
