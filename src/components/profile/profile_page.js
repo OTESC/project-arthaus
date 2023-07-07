b981b3071767af1b26a6df3e4639f4bd08f87e71
Taiwo Tom-Ayegunle <taiwotom-ayegunle@Taiwos-MacBook-Pro.local>
2023-07-07 23:40:15 +0100

    profile commit 7

--- src/components/profile/profile_page.js
+++ src/components/profile/profile_page.js
@@ -5,7 +5,6 @@ import PostsSaved from "./posts_saved";
 import Settings from "./settings";
 import UserBio from "./user_bio";
 import "./profile.css"
-// import "../../bootstrap-5.3.0-alpha3-dist/css/bootstrap.css";
 const ProfilePage = () => {
   return (
     <div>
--- src/components/profile/settings.js
+++ src/components/profile/settings.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import SettingsIcon from "../../assets/profile_images/settings_icon.png"
+import SettingsDropDownIcon from "../../assets/profile_images/settings_dropdown_icon.png"
+import PersonalDetailsIcon from "../../assets/profile_images/personal_details_icon.png"
+import faqIcon from "../../assets/profile_images/faq-icon.png"
 const Settings = () => {
     const [dropdown, setDropDown] = useState('');
     const toggleSettings = () => {
@@ -10,12 +13,19 @@ const Settings = () => {
     let settingsMenu = <div className="container">
         <div className="sub_settings_wrap" id="subSettings">
             <div class="sub_settings">
-                <div className="faq flex py-5">
-                    <img src={SettingsIcon} alt="faq_icon"></img>
-                    <p>FAQ</p>
+            <div className="settings_drop_icon flex py-5 text-left">
+                <img src={SettingsDropDownIcon} alt="settings_dropdown_icon"></img>
+                    <h3>SETTINGS</h3>
                 </div>
-
-
+                <div className="personal_details_icon flex py-5">
+                    <img src={PersonalDetailsIcon} alt="personal_details_icon"></img>
+                    <p>Personal Details</p>
+                </div>
+                <div className="faq_icon flex py-5">
+                    <img src={faqIcon} alt="faq_icon"></img>
+                    <p>Frequently Asked Questions</p>
+                </div>
+        
             </div>
         </div>
     </div>
