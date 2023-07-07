016dfe4d6c37c374067a9a2f226da722fed9e460
Taiwo Tom-Ayegunle <taiwotom-ayegunle@Taiwos-MacBook-Pro.local>
2023-07-07 19:12:40 +0100

    profile commit 5

--- src/components/profile/add_post.js
+++ src/components/profile/add_post.js
@@ -1,7 +1,15 @@
 import React from "react";
+import AddPostIcon from "../../assets/profile_images/add_post_icon.png";
 const AddPost = () => {
-    return (
-        <div></div>
-    )
-}
+  return (
+    <div>
+      <img
+        src={AddPostIcon}
+        className="img-fluid add_post_icon"
+        alt="add_post_icon"
+        onClick="{}"
+      ></img>
+    </div>
+  );
+};
 export default AddPost;
--- src/components/profile/posts_saved.js
+++ src/components/profile/posts_saved.js
@@ -1,87 +1,85 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import AI from "../../assets/art/AI.png";
-import giraffe from "../../assets/art/jiraffe.png"
-import scenery from "../../assets/art/scenery.png"
-import scrapper from "../../assets/art/scrapper.png"
-import scrappers from "../../assets/art/scrappers.png"
-import smiley from "../../assets/art/smiley.png"
-import vr from "../../assets/art/vr.png"
+import giraffe from "../../assets/art/jiraffe.png";
+import scenery from "../../assets/art/scenery.png";
+import scrapper from "../../assets/art/scrapper.png";
+import scrappers from "../../assets/art/scrappers.png";
+import smiley from "../../assets/art/smiley.png";
+import vr from "../../assets/art/vr.png";
 import AI_dark from "../../assets/profile_images/AI_dark.png";
 import giraffe_dark from "../../assets/profile_images/giraffe_dark.png";
 import scrappers_dark from "../../assets/profile_images/scrappers_dark.png";
 import smiley_dark from "../../assets/profile_images/smiley_dark.png";
 
 const PostsSaved = () => {
+  const posted_images = [giraffe, scenery, scrappers, AI, vr, smiley, scrapper];
+  const saved_images = [giraffe_dark, AI_dark, smiley_dark, scrappers_dark];
 
-    const posted_images = [
-      giraffe, scenery, scrappers, AI, vr, smiley, scrapper
-      ];
-      const saved_images = [giraffe_dark, AI_dark, smiley_dark, scrappers_dark];
-
-      
-      
-
-      const postedImages = 
-      <div data-masonry='{"percentPosition": true}' className="posted_images">
-  <div className="col">
-  <img src={posted_images[0]} className="img-thumbnail" alt="8" /> 
-  <img src={posted_images[1]} className="img-thumbnail" alt="12" />
-  <img src={posted_images[2]} className="img-thumbnail" alt="14" /> 
-    
-  </div>
-  <div className="col">
-  <img src={posted_images[3]} className="img-thumbnail" alt="1" /> 
-  <img src={posted_images[4]} className="img-thumbnail" alt="18" />
-    
-  </div>
-  <div className="col">
-  <img src={posted_images[5]} className="img-thumbnail" alt="15" /> 
-  <img src={posted_images[6]} className="img-thumbnail" alt="13" /> 
-  </div>
-</div>
-  
-
-const [postedContent , setPostedContent ] = useState("");
-const clickPosted = () => {
-    
-  setPostedContent ((postedContent === "") ?[postedImages] :"")
-}
-
-      const savedImages = <div>
-        <div data-masonry='{"percentPosition": true}' className="saved_images ">
-          <div className="col">
-            <p className="all_posts">All Posts</p>
-
-            <img src={saved_images[0]} className="img-thumbnail" alt="8" />
-            <p className="portraits">Portraits</p>
-
-            <img src={saved_images[2]} className="img-thumbnail" alt="12" />
-          </div>
-
-          <div className="col">
-            <p className="AI">Artificial Intelligence</p>
-            <img src={saved_images[1]} className="img-thumbnail" alt="1" />
-            <p className="architecture">Architecture</p>
-            <img src={saved_images[3]} className="img-thumbnail" alt="18" />
-          </div>
-        </div>
-
+  const postedImages = (
+    <div data-masonry='{"percentPosition": true}' className="posted_images">
+      <div className="col">
+        <img src={posted_images[0]} className="img-thumbnail" alt="8" />
+        <img src={posted_images[1]} className="img-thumbnail" alt="12" />
+        <img src={posted_images[2]} className="img-thumbnail" alt="14" />
       </div>
+      <div className="col">
+        <img src={posted_images[3]} className="img-thumbnail" alt="1" />
+        <img src={posted_images[4]} className="img-thumbnail" alt="18" />
+      </div>
+      <div className="col">
+        <img src={posted_images[5]} className="img-thumbnail" alt="15" />
+        <img src={posted_images[6]} className="img-thumbnail" alt="13" />
+      </div>
+    </div>
+  );
 
-const [savedContent , setSavedContent ] = useState("");
-const clickSaved = () => {
-    
-  setSavedContent((savedContent === "") ?[savedImages] :"")
-}
+  const [postedContent, setPostedContent] = useState([postedImages]);
+  const clickPosted = () => {
+    setPostedContent(postedContent === [postedImages] ? "" : [postedImages]);
+  };
 
-    return (
-        <div className="d-flex pl-2">
-            <div><button type="button" className="postsbutton" onClick={clickPosted}>Posts</button>{postedContent}</div>
-            <div><button type="button" className="savedbutton" onClick={clickSaved}>Saved </button>{savedContent}</div>
-        
+  const savedImages = (
+    <div>
+      <div data-masonry='{"percentPosition": true}' className="saved_images ">
+        <div className="col">
+          <p className="all_posts">All Posts</p>
 
-            
+          <img src={saved_images[0]} className="img-thumbnail" alt="8" />
+          <p className="portraits">Portraits</p>
+
+          <img src={saved_images[2]} className="img-thumbnail" alt="12" />
         </div>
-    )
-}
+
+        <div className="col">
+          <p className="AI">Artificial Intelligence</p>
+          <img src={saved_images[1]} className="img-thumbnail" alt="1" />
+          <p className="architecture">Architecture</p>
+          <img src={saved_images[3]} className="img-thumbnail" alt="18" />
+        </div>
+      </div>
+    </div>
+  );
+
+  const [savedContent, setSavedContent] = useState([savedImages]);
+  const clickSaved = () => {
+    setSavedContent(savedContent === [savedImages] ? "" : [savedImages]);
+  };
+
+  return (
+    <div className=" pl-2">
+      <div>
+        <button type="button" className="postsbutton" onClick={clickPosted}>
+          Posts
+        </button>
+        {postedContent}
+      </div>
+      <div>
+        <button type="button" className="savedbutton" onClick={clickSaved}>
+          Saved{" "}
+        </button>
+        {savedContent}
+      </div>
+    </div>
+  );
+};
 export default PostsSaved;
